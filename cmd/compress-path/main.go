5ea75d2224a0e2057e46ab45f63ecd4d64760fab
Patrick Organ <patrick@permafrost.dev>
2022-10-07 06:26:34 -0400

    wip

--- cmd/compress-path/main.go
+++ cmd/compress-path/main.go
@@ -5,17 +5,13 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/dnnrly/abbreviate/data"
 	"github.com/dnnrly/abbreviate/domain"
 )
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Println("compress-path " + Version)
-		os.Exit(0)
-	}
-
 	var (
 		matcher     *domain.Matcher
 		abbreviator domain.Abbreviator
@@ -30,26 +26,44 @@ func main() {
 
 	homeDir, homeDirErr := os.UserHomeDir()
 
-	if homeDirErr == nil && strings.Contains(currentPath, homeDir) {
+	if homeDirErr == nil && strings.HasPrefix(currentPath, homeDir) {
 		currentPath = strings.Replace(currentPath, homeDir, "~", 1)
 	}
 
-	newPath := currentPath
-
-	if len(currentPath) > 15 {
-		matcher = data.Abbreviations["en-us"]["common"]
-		abbreviator = matcher
-
-		parts := strings.Split(currentPath, string(os.PathSeparator))
-
-		for i, v := range parts {
-			if i < len(parts)-1 && len(v) > 5 {
-				parts[i] = abbreviator.Abbreviate(parts[i])
-			}
-		}
-
-		newPath = strings.Join(parts, string(os.PathSeparator))
+	if len(currentPath) <= 15 {
+		fmt.Printf("%s", currentPath)
+		os.Exit(0)
 	}
+
+	matcher = data.Abbreviations["en-us"]["common"]
+	abbreviator = matcher
+
+	parts := strings.Split(currentPath, string(os.PathSeparator))
+	newParts := make([]string, len(parts))
+	var wg sync.WaitGroup
+	wg.Add(len(parts))
+
+	for i, v := range parts {
+		go func(i int, v string) {
+			defer wg.Done()
+			if i < len(newParts)-1 && len(v) > 5 {
+				newParts[i] = abbreviator.Abbreviate(v)
+			} else {
+				newParts[i] = v
+			}
+
+			if i == len(newParts)-1 {
+				newParts[i] = "\033[38;5;159m\033[1m" + newParts[i] + "\033[22m\033[38;5;0m"
+			} else {
+				newParts[i] = "\033[38;5;123m" + newParts[i] + "\033[38;5;0m"
+			}
+		}(i, v)
+	}
+
+	wg.Wait()
+	// sep := string(os.PathSeparator)
+	sep := "\033[38;5;255m\ue216\033[38;5;0m"
+	newPath := strings.Join(newParts, sep)
 
 	fmt.Printf("%s", newPath)
 }
